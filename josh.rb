7a8f9a1180ef37a0ac171b13f6f737fdcf4d6fa7
Josh Cheek <josh.cheek@gmail.com>
2017-02-27 16:20:13 -0500

    Refactor for challenge 2

--- josh.rb
+++ josh.rb
@@ -1,19 +1,39 @@
+require 'strscan'
+
 class Lisp
   def self.eval(str)
-    parse str
+    ast = parse str
+    Lisp.new.eval(ast)
   end
 
   def self.parse(str)
-    case str
-    when /\A\d+\z/
-      str.to_i
-    when '#t'
-      true
-    when '#f'
-      false
-    else
-      raise "can't parse: #{str.inspect}"
+    lex tokenize str
+  end
+
+  def self.tokenize(str)
+    scanner = StringScanner.new(str)
+    tokens  = []
+    until scanner.eos?
+      case
+      when scanner.scan(/\d+/)
+        tokens << scanner.matched.to_i
+      when scanner.scan(/#t\b/)
+        tokens << true
+      when scanner.scan(/#f\b/)
+        tokens << false
+      else
+        raise "Can't scan #{scanner.rest}"
+      end
     end
+    tokens
+  end
+
+  def self.lex(tokens)
+    tokens
+  end
+
+  def eval(ast)
+    ast.first
   end
 end
 
@@ -28,12 +48,26 @@ RSpec.describe 'Challenges' do
     expect(Lisp.eval lisp).to eq ruby
   end
 
-  it 'evaluates numbers' do
-    assert_eval "1", 1
-    assert_eval "2", 2
+  describe 'Challenge 1' do
+    it 'evaluates numbers' do
+      assert_eval "1", 1
+      assert_eval "2", 2
+    end
+    it 'evaluates booleans' do
+      assert_eval "#t", true
+      assert_eval "#f", false
+    end
   end
-  it 'evaluates booleans' do
-    assert_eval "#t", true
-    assert_eval "#f", false
+
+  describe 'Challenge 2' do
+    it 'evaluates simple addition' do
+      assert_eval "(+ 1)", 1
+      assert_eval "(+ 1 2)", 3
+      assert_eval "(+ 1 2 10)", 13
+    end
+    it 'evaluates simple multiplicatione' do
+      assert_eval "(* 2)", 2
+      assert_eval "(* 2 2 3)", 12
+    end
   end
 end
