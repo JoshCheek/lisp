120f85044b89afc7fe0e054cf8bde1868f3c523f
Josh Cheek <josh.cheek@gmail.com>
2017-02-27 16:41:24 -0500

    Passes challenge 3

--- josh.rb
+++ josh.rb
@@ -48,6 +48,7 @@ class Lisp
 
   def eval_list(list)
     name, *args = list
+    args = args.map { |arg| eval arg }
     if name == :+
       args.reduce(0, :+)
     elsif name == :*
@@ -82,7 +83,7 @@ RSpec.describe 'Challenges' do
   end
 
   describe 'Challenge 2' do
-    it 'evaluates simple addition', t:true do
+    it 'evaluates simple addition' do
       assert_eval "(+ 1)", 1
       assert_eval "(+ 1 2)", 3
       assert_eval "(+ 1 2 10)", 13
@@ -92,4 +93,85 @@ RSpec.describe 'Challenges' do
       assert_eval "(* 2 2 3)", 12
     end
   end
+
+  describe 'Challenge 3' do
+    it 'evaluates nested addition and multiplication calls' do
+      assert_eval "(+ 1 (* 2 3))", 7
+    end
+  end
 end
+
+__END__
+## Challenge 4
+
+Evaluate conditionals:
+
+```
+lisp_eval("(if #t 1 2)").should == 1
+lisp_eval("(if #f #t #f)").should == false
+```
+
+
+## Challenge 5
+
+Evaluate top-level definitions:
+
+```
+lisp_eval("(def x 3)
+           (+ x 1)").should == 4
+```
+
+## Challenge 6
+
+Evaluate simple `let` bindings:
+
+```
+lisp_eval("(let (x 3)
+             x)").should == 3
+```
+
+## Challenge 7
+
+Evaluate more sophisticated `let` bindings:
+
+```
+lisp_eval("(let (x 3)
+             (+ x 1))").should == 4
+```
+
+## Challenge 8
+
+Evaluate `let` bindings with multiple variables:
+
+```
+lisp_eval("(let (x 3
+                 y 4)
+             (+ x y))").should == 7
+```
+
+## Challenge 9
+
+Evaluate function definitions:
+
+```
+code = "(defn add2 (x)
+          (+ x 2))
+
+        (add2 9)"
+
+lisp_eval(code).should == 1
+```
+
+## CHALLENGE 10
+Evaluates function definitions with multiple variables:
+
+```
+code = "(defn maybeAdd2 (bool x)
+          (if bool
+            (+ x 2)
+            x))
+
+        (+ (maybeAdd2 1 #t) (maybeAdd2 1 #f))"
+
+lisp_eval(code).should == 4
+```
